5cdd7d86b8b896a4748b4481e61b983cdc2d90ee
Doni1124 <jannatulferdousayesha24@gmail.com>
2026-09-15 23:52:17 +0600

    Update MACchanger.py

--- MACchanger.py
+++ MACchanger.py
@@ -1,10 +1,16 @@
 import subprocess
 
-interface_name = input("Enter your interface name: ")
-mac_address = input("Enter MAC address name: ")
+try:
+    interface_name = input("Enter your interface name: ")
+    mac_address = input("Enter MAC address: ")
+except KeyboardInterrupt:
+    print("\nexit")
+    exit()
+except Exception:
+    print("\nexit")
+    exit()
+subprocess.run("ifconfig " + interface_name + " down", shell= True)
+subprocess.run("ifconfig " + interface_name + " hw ether " + mac_address, shell=True)
+subprocess.run("ifconfig " + interface_name + " up", shell=True)
 
-subprocess.run("ifconfig " + interface_name + " down", shell = True)
-subprocess.run("ifconfig " + interface_name + " hw ether " + mac_address, shell = True)
-subprocess.run("ifconfig " + interface_name + " up", shell = True)
-
-print("MAC-address changed succesfully")
+print("mac address changed succesfully")
